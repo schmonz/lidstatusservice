cf6a4540231e11f97297b08c88048c70f4abde60
Amitai Schleier <schmonz-web-git@schmonz.com>
2024-06-11 15:30:28 -0400

    Add recommended boilerplate for start/stop status. Still can't stop.

--- LidStatusService/LidStatusService.cs
+++ LidStatusService/LidStatusService.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.ServiceProcess;
 
 namespace LidStatusService
 {
     public partial class LidStatusService : ServiceBase
     {
+        public enum ServiceState
+        {
+            ServiceStopped = 0x00000001,
+            ServiceStartPending = 0x00000002,
+            ServiceStopPending = 0x00000003,
+            ServiceRunning = 0x00000004,
+            ServiceContinuePending = 0x00000005,
+            ServicePausePending = 0x00000006,
+            ServicePaused = 0x00000007
+        }
+
         private Lid _lid;
 
         public LidStatusService()
@@ -13,11 +25,25 @@ namespace LidStatusService
             InitializeComponent();
         }
 
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
         protected override void OnStart(string[] args)
         {
+            var serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.ServiceStartPending,
+                dwWaitHint = 100000
+            };
+            SetServiceStatus(ServiceHandle, ref serviceStatus);
+
             Log("{0}: service running", DateTime.Now);
             _lid = new Lid();
-            Log("{0}: notifications registered? {1}", DateTime.Now, _lid.RegisterLidEventNotifications(ServiceHandle, ServiceName, LidEventHandler));
+            Log("{0}: notifications registered? {1}", DateTime.Now,
+                _lid.RegisterLidEventNotifications(ServiceHandle, ServiceName, LidEventHandler));
+
+            serviceStatus.dwCurrentState = ServiceState.ServiceRunning;
+            SetServiceStatus(ServiceHandle, ref serviceStatus);
         }
 
         private void LidEventHandler(bool status)
@@ -27,7 +53,17 @@ namespace LidStatusService
 
         protected override void OnStop()
         {
+            var serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.ServiceStopPending,
+                dwWaitHint = 100000
+            };
+            SetServiceStatus(ServiceHandle, ref serviceStatus);
+
             Log("{0}: notifications unregistered? {1}", DateTime.Now, _lid.UnregisterLidEventNotifications());
+
+            serviceStatus.dwCurrentState = ServiceState.ServiceStopped;
+            SetServiceStatus(ServiceHandle, ref serviceStatus);
         }
 
         private static void Log(string format, params object[] arg)
@@ -36,6 +72,18 @@ namespace LidStatusService
             {
                 sw.WriteLine(format, arg);
             }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
         }
     }
 }
